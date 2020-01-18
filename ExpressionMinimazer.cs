31ed2a5540331ea14ef15ec886d6ef2dfa1536d3
Maks <klavioturasucka2@gmail.com>
2020-01-18 16:03:44 +0900

    Parser0.2

--- ExpressionMinimazer.cs
+++ ExpressionMinimazer.cs
@@ -11,17 +11,40 @@ namespace Parser
         string EarlyExpress;
         public string Result { get; set; }
 
-        public ExpressionMinimazer() { }
+        public ExpressionMinimazer()
+        {
+            EarlyExpress = "";
+            Result = "";
+        }
 
         //Минимизачия вводимого выражения 
         //С учетом его предыдущего состояния и результата этого состояния
         public string Minimize(string Expression)
         {
-            if (Result == "") return Expression;
+            if (Result == "" || Expression.Length < EarlyExpress.Length)
+            {
+                EarlyExpress = Expression;
+                return Expression;
+            }
 
+            string NewPeice = Expression.Substring(EarlyExpress.Length, Expression.Length - 1);
 
+            if (!StrIsNum(ref NewPeice))
+            {
+                NewPeice = Expression;
+                Expression = Expression.Replace(EarlyExpress, Result);
+                EarlyExpress = NewPeice;
+            }
 
             return Expression;
+        }
+
+
+        private bool StrIsNum(ref string str)
+        {
+            for (byte i = 0; i < str.Length; i++)
+                if (!Char.IsDigit(str[i])) return false;
+            return true;
         }
     }
 }
